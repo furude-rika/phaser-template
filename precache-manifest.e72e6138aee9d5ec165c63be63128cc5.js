20225dbf931d63cd45ddaf4912de77c1ed91df6f
Mark <mark@remarkablemark.org>
2020-07-03 23:10:21 -0400

    gitploy

---
+++ precache-manifest.e72e6138aee9d5ec165c63be63128cc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f1b80bf4d2fdf51b94ebc5f33caf31",
+    "url": "/phaser-template/index.html"
+  },
+  {
+    "revision": "6e5ec93e4e667dd1eee5",
+    "url": "/phaser-template/static/css/main.a421fb84.chunk.css"
+  },
+  {
+    "revision": "6e5ec93e4e667dd1eee5",
+    "url": "/phaser-template/static/js/main.de3602a0.chunk.js"
+  },
+  {
+    "revision": "e2cf44ede386fd6e5ca9",
+    "url": "/phaser-template/static/js/runtime-main.0f51f80c.js"
+  },
+  {
+    "revision": "05fd3ce5329a2268229eaf44910bf72d",
+    "url": "/phaser-template/static/media/dude.526a78a9.png"
+  },
+  {
+    "revision": "fc7d654a5cbca97fbe6091f9b37ac135",
+    "url": "/phaser-template/static/media/platform.69db09bf.png"
+  },
+  {
+    "revision": "cb8fe21064e648d4fd4aa71e2a5d339d",
+    "url": "/phaser-template/static/media/sky.47a86b19.png"
+  },
+  {
+    "revision": "3bbfad7412f062fcbae5984254093294",
+    "url": "/phaser-template/static/media/star.a7cb585c.png"
+  }
+]);
